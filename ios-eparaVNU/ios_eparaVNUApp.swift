2c2cc46fda75d7915aeba82f9609ed91c6a68c87
sakhnenkoff <39955656+sakhnenkoff@users.noreply.github.com>
2022-04-09 17:26:57 +0300

    Initial Commit

---
+++ ios-eparaVNU/ios_eparaVNUApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_eparaVNUApp.swift
+//  ios-eparaVNU
+//
+//  Created by Matthew Sakhnenko on 09.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_eparaVNUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
